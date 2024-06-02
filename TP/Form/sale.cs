6fe9cd219a3185ddc698589c2dd47d99aaaef242
Dongsu Yoo <yoods12@naver.com>
2024-06-02 22:44:03 +0900

    판매 수정

--- TP/Form/sale.cs
+++ TP/Form/sale.cs
@@ -21,22 +21,58 @@ namespace TP
             InitializeComponent();
         }
 
-        private void creditCard_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button6_Click(object sender, EventArgs e)
-        {
-
-        }
-
         private void cash_Click(object sender, EventArgs e)
         {
 
         }
+        private void allCancel_Click(object sender, EventArgs e)
+        {
 
+        }
         private void chupachups_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void paperCup_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cupOfIce_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void selectionCancel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void plasticBackSmall_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void plasticBackMedium_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void productList_Click(object sender, EventArgs e)
+        {
+            // 판매 폼을 생성하고 표시합니다
+            using (Form form = new Stock())
+            {
+                // ShowDialog()를 사용해 판매 폼을 모달로 표시합니다.
+                form.ShowDialog();
+
+                // 판매 폼이 닫히면 메인 폼을 다시 표시합니다.
+                this.Show();
+            }
+        }
+
+        private void pay_Click(object sender, EventArgs e)
         {
 
         }
