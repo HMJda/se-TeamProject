e92ecb986651327bc3373fda163614eb1d787af9
HMJda <hmj4669@gmail.com>
2024-06-07 01:53:55 +0900

    필요없는 부분 삭제

--- TP/Entitiy/ReceiptList.cs
+++ TP/Entitiy/ReceiptList.cs
@@ -10,27 +10,26 @@ namespace TP.Entitiy
 {
     internal class ReceiptList
     {
-        private string DB_Server_Info;
         private DBController dBcontroller;
-        private string sqltxt = "select * from 영수증";
-        private string dbServerInfo = "Data Source = localhost; User ID = DEU; Password = 1234;";
+        private DataTable receiptTable;
 
         public ReceiptList()
         {
-            DB_Server_Info = dbServerInfo;
             dBcontroller = new DBController();
         }
 
         public DataTable GetReceipt(string sqltxt)
         {
-            return dBcontroller.GetDB(sqltxt);
+            receiptTable = dBcontroller.GetDB(sqltxt);
+            return receiptTable;
         }
 
         public DataTable GetReceiptDetails(string receiptNo)
         {
             // 영수증 상세 정보를 가져오는 쿼리 수정
             string sqltxt = $"SELECT * FROM 영수증상세 WHERE 영수증번호 = '{receiptNo}'";
-            return dBcontroller.GetDB(sqltxt);
+            receiptTable = dBcontroller.GetDB(sqltxt);
+            return receiptTable;
         }
 
         public void SaveRefundedReceipt(string receiptNo)
