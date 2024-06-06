658142ed1f5e442dc7cdd9bbd25c0dd8749573cc
HMJda <hmj4669@gmail.com>
2024-06-06 14:59:10 +0900

    재고 등록에 발주 정보 불러오는거 추가

--- TP/Form/AddInvenUI.cs
+++ TP/Form/AddInvenUI.cs
@@ -1,15 +1,24 @@
 ﻿using System;
+using System.Data;
+using System.Data.OracleClient;
 using System.Windows.Forms;
+using TP.control;
 
 
 namespace TP
 {
     public partial class AddInvenUI : Form
     {
+        private OrderReturnController OrderController;
+        private StockController stckcontroller;
+        private DataTable dt;
+
         string DB_Server_Info = "Data Source = localhost; User ID = system; Password = 1234;";
         public AddInvenUI()
         {
             InitializeComponent();
+            OrderController = new OrderReturnController();
+            dataview();
         }
 
         private void quitButton_Click(object sender, EventArgs e)
@@ -44,6 +53,37 @@ namespace TP
             // 등록된 물품 정보와 개수 변동 사항을 메시지 박스에 표시
             string message = $"상품이 등록되었습니다.\n\n상품 코드: {productCode}\n상품명: {productName}\n등록된 개수: {quantity}개";
             MessageBox.Show(message, "등록 완료", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void dataview()
+        {
+            try
+            {
+                addInvenTable.Columns.Clear();
+                dt = OrderController.GetOrders("select 발주제품,제품.제품명,수량,제품.규격,제품.제조업체 from 발주,제품 where 발주.발주제품=제품.제품번호");
+                addInvenTable.AllowUserToAddRows = false; //빈레코드 표시x
+                var chkCol = new DataGridViewCheckBoxColumn
+                {
+                    Name = "chk",
+                    HeaderText = "선택"
+                };
+                addInvenTable.Columns.Add(chkCol);
+                addInvenTable.DataSource = dt;   //데이터 추가 부분
+
+                //크기 조절부분 
+                addInvenTable.Columns[0].Width = 40;
+
+                // dataGridView1.ReadOnly = true; //전부 읽기 전용
+                for (int i = 1; i < addInvenTable.Columns.Count; i++)
+                {
+                    addInvenTable.Columns[i].ReadOnly = true;
+                }
+
+            }
+            catch (OracleException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
