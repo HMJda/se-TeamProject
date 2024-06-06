45f35f83afbf5f060a033d03eecf2e6ac9839da3
U <prie4653@naver.com>
2024-06-06 14:56:47 +0900

    receipt 수정본

--- TP/Entitiy/ReceiptList.cs
+++ TP/Entitiy/ReceiptList.cs
@@ -1,37 +1,118 @@
-﻿using Oracle.ManagedDataAccess.Client;
-using System.Data;
+﻿using System.Text;
+using System.Threading.Tasks;
 using TP.control;
+using Oracle.ManagedDataAccess.Client;
+using System.Collections.Generic;
+using System.Data;
+using System;
 
 namespace TP.Entitiy
 {
     internal class ReceiptList
     {
-        private DBController dbController;
+        private string DB_Server_Info;
+        private DBController dBcontroller;
         private DataTable ReceiptTable;
         private string sqltxt = "select * from 영수증";
 
+        // 생성자를 통해 DB 연결 정보를 주입받도록 변경
+        public ReceiptList(string dbServerInfo)
+        {
+            DB_Server_Info = dbServerInfo;
+        }
         public ReceiptList()
         {
-            dbController = new DBController();
-            ReceiptTable = new DataTable();
+            dBcontroller = new DBController();
+        }
+
+        public OracleDataReader ReadReceipts() // 읽기만 하는 용도
+        {
+            using (OracleConnection conn = new OracleConnection(DB_Server_Info))
+            {
+                conn.Open();
+                OracleCommand cmd = new OracleCommand(sqltxt, conn);
+                return cmd.ExecuteReader();
+            }
+        
         }
         public DataTable GetReceipt()
         {
-            ReceiptTable = dbController.GetDB(sqltxt);
+            ReceiptTable = dBcontroller.GetDB(sqltxt);
             return ReceiptTable;
+        }
+
+        public DataTable GetReceipts() // 영수증 정보 모두 받는 용도
+        {
+            DBController dBController = new DBController();
+            return dBController.GetDB("select * from 영수증");
+        }
+
+        public bool IsReceiptExists(int receiptNumber)
+        {
+            using (OracleDataReader receiptReader = ReadReceipts())
+            {
+                while (receiptReader.Read())
+                {
+                    int db_receiptNumber = Convert.ToInt32(receiptReader["영수증번호"]);
+                    if (db_receiptNumber == receiptNumber)
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+        }
+
+        public Dictionary<string, string> GetReceiptInfo(int receiptNumber)
+        {
+            using (OracleDataReader receiptReader = ReadReceipts())
+            {
+                while (receiptReader.Read())
+                {
+                    int db_receiptNumber = Convert.ToInt32(receiptReader["영수증번호"]);
+                    if (db_receiptNumber == receiptNumber)
+                    {
+                        // 필드 데이터 읽기
+                        string transactionTime = receiptReader["거래시간"].ToString();
+                        string transactionType = receiptReader["거래형태"].ToString();
+                        string totalPrice = receiptReader["총가격"].ToString();
+
+                        // 영수증 정보를 딕셔너리에 저장
+                        return new Dictionary<string, string>
+                        {
+                            { "영수증번호", receiptNumber.ToString() },
+                            { "거래시간", transactionTime },
+                            { "거래형태", transactionType },
+                            { "총가격", totalPrice }
+                        };
+                    }
+                }
+                return null; // 매칭되는 영수증이 없을 경우 null 반환
+            }
+        }
+
+        public void SaveRefundedReceipt(int receiptNo)
+        {
+            string sqltxt = $"INSERT INTO RefundedReceipts (ReceiptNo, RefundDate) VALUES ({receiptNo}, CURRENT_TIMESTAMP)";
+            DBController dBController = new DBController();
+            dBController.SetDB(sqltxt);
         }
         public DataTable GetReceipt(string sqltxt)
         {
-            ReceiptTable = dbController.GetDB(sqltxt);
+
+            ReceiptTable = dBcontroller.GetDB(sqltxt);
             return ReceiptTable;
         }
+
         public void SetReceipt(string sqltxt)
         {
-            dbController.SetDB(sqltxt);
-        }
-        public void SetReceipt(string sqltxt, OracleParameter[] parameters)
-        {
-            dbController.ExecuteNonQuery(sqltxt, parameters);
+            using (OracleConnection conn = new OracleConnection(DB_Server_Info))
+            {
+                conn.Open();
+                OracleCommand cmd = new OracleCommand(sqltxt, conn);
+                cmd.ExecuteNonQuery();
+            }
+            dBcontroller.SetDB(sqltxt);
         }
     }
 }
---
+++ TP/control/RefundController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Oracle.ManagedDataAccess.Client;
+using TP.Entitiy;
+
+namespace TP.control
+{
+    internal class RefundController
+    {
+        private ReceiptList receiptList;
+        private DateTime selectedDate;
+        private int selectedReceiptNumber;
+        private DateTimePicker dateTimePicker;
+        private TextBox receiptNumberTextBox;
+
+        public RefundController(string dbServerInfo, DateTimePicker dateTimePicker, TextBox receiptNumberTextBox)
+        {
+            this.dateTimePicker = dateTimePicker;
+            this.receiptNumberTextBox = receiptNumberTextBox; // 텍스트 상자에 대한 참조 저장
+            receiptList = new ReceiptList(dbServerInfo);
+            selectedDate = GetDate();
+            selectedReceiptNumber = GetReceiptNumber();
+        }
+
+        private DateTime GetDate()
+        {
+            DateTime selectedDate = dateTimePicker.Value;
+            return selectedDate;
+        }
+
+        private int GetReceiptNumber()
+        {
+            // 사용자가 입력한 영수증 번호를 텍스트 상자에서 가져오는 로직
+            int receiptNumber;
+            if (!int.TryParse(receiptNumberTextBox.Text, out receiptNumber))
+            {
+                // 사용자가 올바른 숫자를 입력하지 않은 경우 처리
+                MessageBox.Show("잘못된 영수증 번호입니다.");
+            }
+            // 10자리 이상의 숫자를 입력한 경우 처리
+            if (receiptNumber.ToString().Length > 10)
+            {
+                MessageBox.Show("잘못된 영수증 번호입니다.");
+            }
+            return receiptNumber;
+        }
+
+        public DataTable GetReceipt()
+        {
+            string sqltxt;
+            if (selectedReceiptNumber == 0)
+            {
+                sqltxt = $"SELECT * FROM 영수증 WHERE Date = TO_DATE('{selectedDate:yyyy-MM-dd}', 'YYYY-MM-DD')";
+            }
+            else
+            {
+                sqltxt = $"SELECT * FROM 영수증 WHERE No = {selectedReceiptNumber} AND Date = TO_DATE('{selectedDate:yyyy-MM-dd}', 'YYYY-MM-DD')";
+            }
+            return receiptList.GetReceipts();
+        }
+
+        public DataTable GetReceiptDetails(int receiptNo)
+        {
+            string sqltxt = $"SELECT * FROM ReceiptDetails WHERE ReceiptNo = {receiptNo}";
+            return receiptList.GetReceipts();
+        }
+
+        public bool ProcessRefund(int receiptNo)
+        {
+            try
+            {
+                // 환불 처리 로직
+                UpdateInventory(receiptNo);
+                SaveRefundedReceipt(receiptNo);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"환불 처리 중 오류 발생: {ex.Message}");
+                return false;
+            }
+        }
+
+        private void UpdateInventory(int receiptNo)
+        {
+            string sqltxt = $"UPDATE 재고 SET Quantity = Quantity + (SELECT Quantity FROM ReceiptDetails WHERE ReceiptNo = {receiptNo}) WHERE ProductId = (SELECT ProductId FROM ReceiptDetails WHERE ReceiptNo = {receiptNo})";
+            receiptList.SetReceipt(sqltxt);
+        }
+
+        private void SaveRefundedReceipt(int receiptNo)
+        {
+            string sqltxt = $"INSERT INTO RefundedReceipts (ReceiptNo, RefundDate) VALUES ({receiptNo}, CURRENT_TIMESTAMP)";
+            receiptList.SetReceipt(sqltxt);
+        }
+    }
+}
